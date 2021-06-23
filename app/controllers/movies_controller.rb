40eae0c77b88cc2f2c7ccfeb0ff6da994de18604
Ryan Searls <Ryan.searls@gmail.com>
2021-06-22 21:04:08 -0500

    added to controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,16 +5,41 @@ def index
   render json: movie.as_json
 end
 
-def first_movie
-  movie = Movie.first
+
+def create
+  movie = Movie.new(
+    title: params[:title],
+    year: params[:year],
+    plot: params[:plot])
+
+  product.save
   render json: movie.as_json
 end 
 
-def second_movie
-  movie = Movie.second
+def show
+  movie_id = params["id"]
+  movie = Movie.find(movie_id)
   render json: movie.as_json
+
 end 
 
+
+def update
+  movie = Movie.find_by(id: params[:id])
+  movie.title = params[:title] || movie.title
+  movie.year = params[:year] || movie.year
+  movie.plot = params[:plot] || movie.plot
+  product.save
+  render json: product.as_json
+end   
+
+
+
+def destroy
+  movie = Movie.find_by(id: params[:id])
+  movie.destroy
+  render json: {message: "Product is destroyed"}
+end 
 
 
 end
