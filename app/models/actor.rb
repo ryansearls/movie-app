85e8e16b899c8bd5e8f7c74c8898a903d291d09d
Ryan Searls <Ryan.searls@gmail.com>
2021-06-25 10:02:52 -0500

    added validations to actors

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,7 @@
 class Actor < ApplicationRecord
 
-  
+  validates :first_name, :last_name, length: { minimum: 2 }
+  validates :known_for, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 13 }
     
 end
