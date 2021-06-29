df6d4c33f9778ca4cfe1756b8f99df1bc6097a10
Ryan Searls <Ryan.searls@gmail.com>
2021-06-28 22:50:05 -0500

    added characters to shawshank

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-  validates :title, :year, :plot, presence: true
-  validates :year, numericality: { only_integer: true }
+  # validates :title, :year, :plot, presence: true
+  # validates :year, numericality: { only_integer: true }
 
   has_many :actors #returns array of many actors
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,24 @@
 
 #my work below
 
-movie = Movie.create(title: "Jumanji", year: 1995, plot: "A magical board game unlea
-  ses a world of adventure on siblings Peter and Judy Shepherd.")
+# movie = Movie.create(title: "Jumanji", year: 1995, plot: "A magical board game unlea
+#   ses a world of adventure on siblings Peter and Judy Shepherd.")
 
-movie = Movie.create(title: "Hardball", year: 2001, plot: "Conor (Keanu Reeves) is a
-  ticket scalper, gambler and, now, Little League coach for a rag-tag team of kids in one of the tough
-  est part of Chicago. He's no role model, but his buddy Jimmy won't help him pay off his gambling debt
-  s unless Conor coaches Jimmy's corporately sponsored team.")
+# movie = Movie.create(title: "Hardball", year: 2001, plot: "Conor (Keanu Reeves) is a
+#   ticket scalper, gambler and, now, Little League coach for a rag-tag team of kids in one of the tough
+#   est part of Chicago. He's no role model, but his buddy Jimmy won't help him pay off his gambling debt
+#   s unless Conor coaches Jimmy's corporately sponsored team.")
 
+
+  actor = Actor.create!(first_name: "Robin", last_name: "Williams", known_for: "Alan Parrish", gender: "M", age: 44 , movie_id: 1)
+
+  actor = Actor.create!(first_name: "Kirsten", last_name: "Dunst", known_for: "Judy Shepherd", gender: "F", age: 14, movie_id: 1)
+  
+  actor = Actor.create!(first_name: "Bebe", last_name: "Neuwirth", known_for: "Nora Shepherd", gender: "F", age: 37, movie_id: 1)
+  
+  
+  actor = Actor.create!(first_name: "Morgan", last_name: "Freeman", known_for: "Ellis Boyd Redding", gender: "M", age: 56, movie_id: 5)
+  
+  actor = Actor.create!(first_name: "Tim", last_name: "Robbins", known_for: "Andy Dufresne", gender: "M", age: 36, movie_id: 18)
+  
   
