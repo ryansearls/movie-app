fe187b524855635564ce42a6875a9baf1aeb5c1d
Ryan Searls <Ryan.searls@gmail.com>
2021-07-02 10:06:33 -0500

    added before_action to movies and actors controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_143834) do
+ActiveRecord::Schema.define(version: 2021_07_02_150049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_143834) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
